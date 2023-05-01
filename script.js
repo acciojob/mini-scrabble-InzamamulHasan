7f2ee537548c1a89a1f4cd655cbc1fd4ef84131f
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-05-01 16:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ let box=document.getElementById("evaluatedText");
 let render=document.getElementById("letterCount");
 box.addEventListener("input",()=>{
 	let str=box.value;
+	if(str.length==0){
+		render.textContent="";
+		return;
+	}
 	render.textContent=str.length;
 })
